d02244e92df815e5feca4b9114c6940ac46d8a09
antunesdetoledobruno-web <antunesdetoledobruno@gmail.com>
2026-01-29 21:44:01 -0300

    Meu codigo testado no playcode.io

---
+++ src/indexV1.js
@@ -0,0 +1,28 @@
+// Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, 
+// depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+// 
+// Se XP for menor do que 1.000 = Ferro
+// Se XP for entre 1.001 e 2.000 = Bronze
+// Se XP for entre 2.001 e 5.000 = Prata
+// Se XP for entre 5.001 e 7.000 = Ouro
+// Se XP for entre 7.001 e 8.000 = Platina
+// Se XP for entre 8.001 e 9.000 = Ascendente
+// Se XP for entre 9.001 e 10.000= Imortal
+// Se XP for maior ou igual a 10.001 = Radiante
+
+//funCTION JOGADOR BRUNAO E NIVEL DE XP{
+LET NICKNAME ("BRUNAO")
+   console.log(NAME + XP)
+ LET(TEM 5500 DE XP)  
+  LET (CONTADORdexP = 500) 
+ LET {("XP-1000 E FERRO 
+    XP +=1000 (BRONZE)
+    XP +=2000(PRATA)
+    XP +=5000(OURO)
+    XP +=7000(PLATINA)
+    XP +=8000(ASCENDENTE)
+    XP +=9000(IMORTAL)
+    XP + 10000(RADIANTE)
+ }   
+}
+ console.log ("PARABENS"+ NAME + NICKMANE XP)
---
+++ src/indexV2.js
@@ -0,0 +1,23 @@
+// Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, 
+// depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+// 
+// Se XP for menor do que 1.000 = Ferro
+// Se XP for entre 1.001 e 2.000 = Bronze
+// Se XP for entre 2.001 e 5.000 = Prata
+// Se XP for entre 5.001 e 7.000 = Ouro
+// Se XP for entre 7.001 e 8.000 = Platina
+// Se XP for entre 8.001 e 9.000 = Ascendente
+// Se XP for entre 9.001 e 10.000= Imortal
+// Se XP for maior ou igual a 10.001 = Radiante
+
+let nomeDoHeroi = "Brunão"
+let xp = 100
+
+if (xp < 1000)
+  console.log("Ferro")
+if (xp >= 1000 && xp < 2000)
+  console.log("Bronze")
+if (xp >= 2000 && xp < 5000)
+  console.log("Prata")
+
+//TODO: Terminar as condições
